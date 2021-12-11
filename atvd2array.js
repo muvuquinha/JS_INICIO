156e0f0f2442ed8b05fedc138339e7b33c1169e1
Drielly Figueiredo <drielly.figueiredoo@gmail.com>
2021-12-10 21:01:42 -0300

    js.arrays

---
+++ atvd2array.js
@@ -0,0 +1,8 @@
+// Alterando um valor do array
+console.clear();
+let moedas = ['Marcia', 'Drielly', 'lol', 'Dolar', 'ilha'];
+
+console.log(`Antes: ${moedas}`);
+moedas[2] = 'presunto';
+moedas[3] = 'queijo';
+console.log(`Depois: ${moedas}`);
